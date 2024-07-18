ae6d2dc0ea168adcd38182a25949db848c45d43c
WilmarTarazona <64029678+WilmarTarazona@users.noreply.github.com>
2024-07-18 11:57:23 -0700

    Update note-app-backend

--- note-app-backend/notes/models.py
+++ note-app-backend/notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.TextField()
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:50]
